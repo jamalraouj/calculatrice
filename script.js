ee1bc622ab583740eaaa27dce4b862760dc015dc
jamalraouj <jamalrwj@gmail.com>
2022-03-08 23:05:22 +0100

    I add interface and operations basic

---
+++ script.js
@@ -0,0 +1,13 @@
+function sum( num1 , num2){
+    var num3 = num1 + num2;
+    window.console.error("You made a mistake");
+    return num3;
+}
+  console.log(sum( 71213 , 5));
+
+
+    var myP = document.createElement('p');
+    myPText = document.createTextNode('this is a paragraph');
+    myP.appendChild(myPText);
+    document.body.appendChild(myP);
+
