943cb747379a2fd84573421ae4db0e3915227c12
jamalraouj <jamalrwj@gmail.com>
2022-03-16 20:01:33 +0100

    j'ai realiser les fonctionalite de calculatrice

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-function sum( num1 , num2){
-    var num3 = num1 + num2;
-    window.console.error("You made a mistake");
-    return num3;
+// function sum( num1 , num2){
+//     var num3 = num1 + num2;
+//     window.console.error("You made a mistake");
+//     return num3;
+// }
+//   console.log(sum( 71213 , 5));
+//
+//
+//     var myP = document.createElement('p');
+//     myPText = document.createTextNode('this is a paragraph');
+//     myP.appendChild(myPText);
+//     document.body.appendChild(myP)
+
+
+
+let s = 10;
+
+for( i = 0 ; i<s ; i+=2){
+    console.log(i);
 }
-  console.log(sum( 71213 , 5));
-
-
-    var myP = document.createElement('p');
-    myPText = document.createTextNode('this is a paragraph');
-    myP.appendChild(myPText);
-    document.body.appendChild(myP);
-
